61a54430f8afdfe84931a239a802a4cb0911c4fa
ZCZDinn <christian.dinn12@ed.cna.nl.ca>
2024-11-23 21:26:58 -0330

    Main Menu/Welcome page formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-/*!
-* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+"use strict";
+
+const $ = selector => document.querySelector(selector);
