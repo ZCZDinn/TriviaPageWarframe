892801b2d61904aacfd6658a987ff7edbe62da12
ZCZDinn <christian.dinn12@ed.cna.nl.ca>
2024-11-23 22:30:13 -0330

    Beginning of additional pages for trivia categories

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 "use strict";
 
 const $ = selector => document.querySelector(selector);
+
+const factionSelect = () => {
+    $("#category_select").value = $("#factions").textContent
+}
+
+const resourceSelect = () => {
+	$("#category_select").value = $("#resources").textContent
+}
+
+const warframeSelect = () => {
+	$("#category_select").value = $("#warframes").textContent
+}
+
+const weaponSelect = () => {
+	$("#category_select").value = $("#weapons").textContent
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	// add event handlers
+	$("#factions").addEventListener("click", factionSelect)
+	$("#resources").addEventListener("click", resourceSelect)
+	$("#warframes").addEventListener("click", warframeSelect)
+	$("#weapons").addEventListener("click", weaponSelect)
+});
